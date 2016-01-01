5da916ac79de6ac9852140c1bc61e0a47ed705bd
David Martin <dmartin.pro@gmail.com>
2016-01-01 23:17:31 +0100

    Able to build an Android version now

--- libtorrent_android.go
+++ libtorrent_android.go
@@ -2,7 +2,7 @@
 
 package libtorrent
 
-// #cgo pkg-config: libtorrent-rasterbar openssl
+// #cgo pkg-config: --static libtorrent-rasterbar openssl
 // #cgo linux CXXFLAGS: -I/usr/include/libtorrent -I/opt/android-toolchain-arm/include/libtorrent -I/opt/android-toolchain-arm/include -Wno-deprecated-declarations
-// #cgo linux LDFLAGS: -lm -ldl -L/opt/android-toolchain-arm/lib
+// #cgo linux LDFLAGS: -nodefaultlibs -lstdc++ -latomic -ldl -lm -lc -L/opt/android-toolchain-arm/lib
 import "C"
